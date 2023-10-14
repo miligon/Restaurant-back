ce1efb368c7a309296bace37b346a02209cac44b
Miguel Limón <mili95_gontex@hotmail.com>
2023-10-14 17:15:01 -0600

    Added test for restaurant

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,7 +5,7 @@ class Restaurant(models.Model):
     id=models.AutoField(verbose_name="Restaurant's ID", primary_key=True, auto_created=True)
     owner=models.ForeignKey(verbose_name="Owner", to=User, on_delete=models.PROTECT)
     name=models.CharField(verbose_name="Name", max_length=100)
-    url=models.SlugField(verbose_name="Restaurant's URL", max_length=100)
+    slug=models.SlugField(verbose_name="Restaurant's URL", max_length=100)
 
     def __str__(self):
         return self.name
--- restaurants/tests.py
+++ restaurants/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+class restaurantsTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.user.save()
+
+        restaurant = Restaurant(
+            name="Restaurant TEST 1",
+            owner=self.user)
+        
+        restaurant.save()
+
+        #Restaurant should augenerate its id
+        self.restaurant = Restaurant.objects.get(pk=1)
+
+    def test_restaurant_has_owner(self):
+        self.assertEqual(self.restaurant.owner.username, "testuser")
+
+    def test_restaurant_shows_name(self):
+        self.assertEqual(str(self.restaurant), "Restaurant TEST 1")
+        

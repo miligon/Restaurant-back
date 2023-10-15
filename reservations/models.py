68c0e71f7ea687427a261a422caab3dc05b9d13c
Miguel Limón <mili95_gontex@hotmail.com>
2023-10-14 18:04:40 -0600

    Added Ticket model and some related test cases

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from restaurants.models import Restaurant
 
-# Create your models here.
+class Tickets(models.Model):
+    code=models.UUIDField(verbose_name="Tickets' code", primary_key=True, auto_created=True)
+    restaurant=models.ForeignKey(verbose_name="Restaurant", to=Restaurant, on_delete=models.PROTECT)
+    name=models.CharField(verbose_name="Description", max_length=50)
+    maxPurchaseCount=models.PositiveIntegerField(verbose_name="Maximum purchase count number")
+    purchaseCount=models.PositiveIntegerField(verbose_name="Purchase count number")
+    
+    def __str__(self):
+        return f'{self.name}({self.code})'
+
+    class Meta:
+        verbose_name="Ticket"
+        verbose_name_plural="Tickets"
--- reservations/tests.py
+++ reservations/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
+from restaurants.models import Restaurant
 
 # Create your tests here.
+class TicketsTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser_tickets', password='12345')
+        self.user.save()
+
+        self.restaurant = Restaurant(
+            name="Restaurant TEST Ticket",
+            owner=self.user)
+        
+        self.restaurant.save()
+
+        self.ticket = Tickets(
+            restaurant=self.restaurant,
+            name="10% OFF",
+            maxPurchaseCount=5,
+            purchaseCount=3
+            )
+        
+        self.ticket.save()
+
+    def test_restaurant_shows_name(self):
+        self.assertEqual(str(self.ticket), "10% OFF")
+        

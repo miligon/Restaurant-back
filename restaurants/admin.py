e1dbf828c4d3312542e4d3408f0a235b863fc14e
Miguel Limón <mili95_gontex@hotmail.com>
2023-10-14 18:29:47 -0600

    Added ModelAdmin for restaurant.Restaurant model

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class RestauranteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner' )
+    search_fields = ( 'name', 'owner')
+    ordering = ('id', 'name', 'owner' )
+    list_filter = ( 'owner')
+
+admin.site.register(Restaurant, RestauranteAdmin)

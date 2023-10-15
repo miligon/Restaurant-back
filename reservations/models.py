f6eda44b3e782f5d77f7a552b686a8aacc7f0ed7
Miguel Limón <mili95_gontex@hotmail.com>
2023-10-14 18:18:51 -0600

    Added a model for purchased tickets

--- reservations/models.py
+++ reservations/models.py
@@ -16,3 +16,14 @@ class Tickets(models.Model):
         verbose_name="Ticket"
         verbose_name_plural="Tickets"
 
+class PurchasedTickets(models.Model):
+    id = models.AutoField(verbose_name="Purchase's ID", primary_key=True, auto_created=True)
+    ticket=models.ForeignKey(verbose_name="Ticket", to=Tickets, on_delete=models.PROTECT)
+    guestName=models.CharField(verbose_name="Guest's Name", max_length=100)
+
+    def __str__(self):
+        return f'{self.guestName}({self.ticket})'
+
+    class Meta:
+        verbose_name="Purchased ticket"
+        verbose_name_plural="Purchased tickets"

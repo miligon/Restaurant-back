f3cf1e6e42ffe856e75563f3dae1d653e0018570
Miguel Limón <mili95_gontex@hotmail.com>
2023-10-15 23:17:03 -0600

    Refactored signals.py from reservations

--- reservations/signals.py
+++ reservations/signals.py
@@ -5,16 +5,11 @@ from .models import Tickets, PurchasedTickets
 @receiver(pre_save, sender=PurchasedTickets)
 def purchasedTicket_created(sender, instance, **kwargs):
     ticket = instance.ticket
-    if ticket.isSoldout():
+    if ticket.getAvailable() <= 0:
         raise Exception("Ticket is soldout!")
     else:
         if (ticket.getAvailable() - instance.quantity < 0):
             raise Exception("Trying to purchase more tickets than available!")
         ticket.purchaseCount += instance.quantity
         ticket.save()
-        ticket.isSoldout()
-        ('Ticket Sold!')
-
-@receiver(pre_save, sender=Tickets)
-def Ticket_save(sender, instance, **kwargs):
-    instance.isSoldout()
+        #print('Ticket Sold!')
